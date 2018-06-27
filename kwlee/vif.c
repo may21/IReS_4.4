e9f4d284ce3dd46b66c8cd5dbac8a6a87e95e774
kwlee_oslab <May@Kyungwoonui-MacBook-Pro.local>
2018-06-27 13:15:35 +0900

    change monitoring period to 1 second

--- kwlee/vif.c
+++ kwlee/vif.c
@@ -99,17 +99,19 @@ static void quota_control(unsigned long data){
 
 #ifdef PRO_SHARE
 	if(credit_allocator->total_credit != 0)
-		total_credit = (credit_allocator->total_credit + (5 - 1))/5;
+		total_credit = credit_allocator->total_credit ;
 	total_weight = credit_allocator->total_weight;
 #endif
 	if(list_empty(&credit_allocator->active_vif_list))
 		goto out;
 
+	credit_allocator->total_credit = 0;
 	list_for_each_entry_safe(temp_vif, next_vif, &credit_allocator->active_vif_list, active_list){
 		if(!temp_vif)
 			goto out;
 
 #if defined(HYBRID)
+		credit_allocator->total_credit += temp_vif->pps;
 		if(credit_allocator->quota_balance > 0)
 			goal = temp_vif->max_credit + (((credit_allocator->quota_balance* temp_vif->weight) + (total_weight-1) )/ total_weight);
 		else
@@ -335,8 +337,10 @@ static void ancs_monitoring(unsigned int data){
 
 		temp_vif->used_credit = 0;
 #endif
+#ifdef PPS_CONTROL
 		result += temp_vif->pps;
 		temp_vif->pps = 0;
+#endif		
 		}
 	credit_allocator->total_credit = result;
 	out:
